dc2526c46be52862907ccffaf0b8dd7d375190ee
spencer <spencer.bigum@gmail.com>
2018-12-25 02:14:19 -0500

    pw changed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,11 +3,12 @@ const {forwardTo} = require('prisma-binding') // gives us the ability to query o
 const Query = {
   // Quick way to mock up api requests and build it out later with AUTH
   items: forwardTo('db'),
+
+  // MANUAL WAY
   // parent - schema of graphQL
   // args - arguments passed into the query
   // ctx - access the db and rest of req(headers, cookies, etc..)
   // info - information on the graphQL query
-  // MANUAL WAY
   // async items(parent, args, ctx, info){
   //   return await ctx.db.query.items()
   // }
